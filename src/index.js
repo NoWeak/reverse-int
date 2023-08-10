1fde21eb473cbda926c20ca7eb708dea6a07ab12
NoWeak <pavelnovik2002@gmail.com>
2023-08-11 00:13:30 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    let newName= '';
+    let i= 0;
+    let s = n.toString();
+       if(n<0){
+        s = s.slice(1,s.length)
+        while(i<s.length){
+            newName =`${s[i]}${newName}`;
+            i++;
+        }
+        return (newName);
+       } else{
+        while(i<s.length){
+            newName =`${s[i]}${newName}`;
+            i++;
+        }
+        return (newName);
+    }
 }
